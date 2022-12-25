3c3b165283ecd452a31fdfef0e0cdc2a101a8100
Best Shi <bestshi.com@gmail.com>
2022-12-25 23:35:04 +0800

    添加README文件内容

--- README.md
+++ README.md
@@ -9,6 +9,14 @@
 ## 已有功能
 
 - 路由跳转
+- Bs 全局对象
+  - config: 项目基础配置。
+  - isAuthenticated: 是否登录，登录为 true，默认：false。
+  - Authorization: 用户授权信息，登录后由后端返回。
+  - menuList: 菜单数组，登录后由后端返回。
+  - sessionStorage: 设置、获取 sessionStorage。
+  - clearSessionStorage: 用于删除、清空 sessionStorage 的值。
+  - MessageBox: 消息确认框。
 
 ## 已有组件
 
--- src/utils/sessionStorage.js
+++ src/utils/sessionStorage.js
@@ -1,4 +1,10 @@
 export const sessionStorage = {
+  /**
+   * 设置、获取 sessionStorage，存在 value 参数时，设置 sessionStorage 值；没有 value 参数时，sessionStorage 的值。
+   * @param {string} key - sessionStorage key 值。
+   * @param {[object,string]} value - sessionStorage value 值。
+   * @returns void
+   */
   sessionStorage(key, value) {
     if (value) {
       window.sessionStorage.setItem(key, JSON.stringify(value));
@@ -11,6 +17,10 @@ export const sessionStorage = {
     // eslint-disable-next-line consistent-return
     return JSON.parse(data);
   },
+  /**
+   * 用于删除、清空 sessionStorage 的值，传入 key 时删除 sessionStorage 中 key 对应的值；key 为空时清空 sessionStorage。
+   * @param {string} key - sessionStorage 的 key 。
+   */
   clearSessionStorage(key) {
     if (key) {
       window.sessionStorage.removeItem(key);
